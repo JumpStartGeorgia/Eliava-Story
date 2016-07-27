3902f2a356735783cf0e63bf9936595bd2d85823
Antarya <antarya@gmail.com>
2016-07-27 10:14:16 +0400

    Generating share

--- assets/locale/en.js
+++ assets/locale/en.js
@@ -10,6 +10,8 @@ en =
   "ge": "Georgian",
   "ru": "Russian",
   "behind": "Behind",
+
+
   "stories": {
     "1": {
       "title": "Story Title 1",
@@ -22,41 +24,16 @@ en =
       "content": "Content 2"
     }
   },
-  "url": "http://dev-eliava.jumpstart.ge/en/",
   "share_sitename": "The Eliava Market",
-  "share_url": "http://dev-eliava.jumpstart.ge/en/share/",
+  "url": "http://dev-eliava.jumpstart.ge",
+  "share_url": "share", // http://dev-eliava.jumpstart.ge/en/share
+
+
   "lang": "ქართული",
   "lang_href": "http://visuals.jumpstart.ge/can_you_buy_a_flat/ka/",
   "lang_abbr": "ka",
   "fb": "http://visuals.jumpstart.ge/can_you_buy_a_flat/assets/images/fb_en.png",
   "site_name": "Can you buy a flat?",
   "description": "Descr",
-  "description_fb": "Explore how long it will take you to purchase a flat or pay off a bank loan based off your monthly savings for cities throughout Georgia.",
-  "question": "Can you buy a flat?",
-  "download": "Download Data",
-  "download_link": "../assets/data/makler_en.zip",
-  "share_result": "It will take me X1 year(s) and X2 month(s) to buy X3 m² flat in X4. What about you?",
-  "m2": "m²",
-  "m2_text": "Flat Area (m²)",
-  "savings": "Monthly Savings (<span>₾</span>)",
-  "city": "City",
-  "georgia": "Georgia",
-  "georgia_capital": "Tbilisi",
-  "no_data": "<br />Data Not Available",
-  "price": "Cost",
-  "year": "year(s)",
-  "month": "month(s)",
-  "js": "JumpStart Georgia - We Communicate Data Better!",
-  "will_take_you": "<br />It will take you",
-  "to_save_up": "to save up",
-  "to_purchase_a_flat": "to purchase a flat.",
-  "or_bank_loan": "Or you can get a bank loan now <br />that will take you",
-  "or_bank_loan_exceed": "Unfortunately, you cannot get a bank loan because it would take you",
-  "or_bank_loan_impossible": "<br />Unfortunately, you cannot get a bank loan because your monthly savings is not enough to cover the monthly loan payments.",
-  "to_pay_off": "to pay off at the cost of",
-  "to_pay_off_exceed": "to pay off at the cost of",
-  "which_is": "which is",
-  "which_is_exceed": "and banks do not give <br />out home loans longer <br />than <span>15 years</span>.",
-  "more_than_cost": "more than the cost of the flat.",
-  "calculator": "Calculate your loan"
+  "description_fb": "Explore how long it will take you to purchase a flat or pay off a bank loan based off your monthly savings for cities throughout Georgia."
 };
